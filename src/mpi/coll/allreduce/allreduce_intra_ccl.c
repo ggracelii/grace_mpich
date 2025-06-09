7df8245b5fc75ef8b6d8b45657023621f3568969
Grace Li <grace.li@anl.gov>
2025-06-09 16:25:21 +0000

    fixed merge conflicts in intra ccl file

--- src/mpi/coll/allreduce/allreduce_intra_ccl.c
+++ src/mpi/coll/allreduce/allreduce_intra_ccl.c
@@ -14,27 +14,29 @@ int MPIR_Allreduce_intra_ccl(const void *sendbuf, void *recvbuf, MPI_Aint count,
                              MPI_Datatype datatype, MPI_Op op, MPIR_Comm * comm_ptr, int ccl,
                              int coll_attr)
 {
-    switch (ccl) {        
+    switch (ccl) {
 #ifdef ENABLE_NCCL
-        case MPIR_CVAR_ALLREDUCE_CCL_nccl:
-            if (MPIR_NCCL_check_requirements_red_op(sendbuf, recvbuf, datatype, op)) {
-                return MPIR_NCCL_Allreduce(sendbuf, recvbuf, count, datatype, op, comm_ptr,
-                                           coll_attr);
-            }
-            break;
+    case MPIR_CVAR_ALLREDUCE_CCL_auto:
+    case MPIR_CVAR_ALLREDUCE_CCL_nccl:
+        if (MPIR_NCCL_check_requirements_red_op(sendbuf, recvbuf, datatype, op)) {
+            return MPIR_NCCL_Allreduce(sendbuf, recvbuf, count, datatype, op, comm_ptr,
+                                       errflag);
+        }
+        break;
 #endif
+
 #ifdef ENABLE_RCCL
-        case MPIR_CVAR_ALLREDUCE_CCL_rccl:
-            if (MPIR_RCCL_check_requirements_red_op(sendbuf, recvbuf, datatype, op)) {
-                return MPIR_RCCL_Allreduce(sendbuf, recvbuf, count, datatype, op, comm_ptr,
-                                           errflag);
-            }
-            break;
+    case MPIR_CVAR_ALLREDUCE_CCL_auto:
+    case MPIR_CVAR_ALLREDUCE_CCL_rccl:
+        if (MPIR_RCCL_check_requirements_red_op(sendbuf, recvbuf, datatype, op)) {
+            return MPIR_RCCL_Allreduce(sendbuf, recvbuf, count, datatype, op, comm_ptr,
+                                       errflag);
+        }
+        break;
 #endif
-        case MPIR_CVAR_ALLREDUCE_CCL_auto:     // Not sure yet how to handle "auto"
-            break; // do I do this???????????????
-        default:
-            goto fallback;
+
+    default:
+        goto fallback;
     }
   fallback:
     return MPIR_Allreduce_allcomm_auto(sendbuf, recvbuf, count, datatype, op, comm_ptr, coll_attr);
